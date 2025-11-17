1fe1a804d5613ba72e7619e8405426c051a27418
Dominika Zgid <dominika.zgid@gmail.com>
2025-11-17 11:22:44 +0100

    Add files via upload
    two files on which we worked recently

---
+++ library_rel_ed.f90
@@ -0,0 +1,284 @@
+subroutine check_orbital_space_declarations(norb,n_RAS_spaces_occ,RAS_space_occ,n_RAS_spaces_virt,RAS_space_virt,active_space,n_alpha,n_beta)
+
+  implicit none
+  integer, intent(in) :: n_alpha, n_beta
+  integer, intent(in) :: norb,n_RAS_spaces_occ,n_RAS_spaces_virt,active_space
+  integer, intent(in) :: RAS_space_occ(n_RAS_spaces_occ),RAS_space_virt(n_RAS_spaces_virt)
+  integer:: orb,i
+
+
+  !norb - total number of orbitals
+  ! the principles for the occupied and virtual RAS spaces are the same
+  !number_RAS_spaces_occ - the number of RAS spaces which are present for occupied orbitals (eg. 3 spaces with core, singly, and doubly occupied orbitals out of total number of occupied orbitals)
+  !dimension_RAS_space_occ(number_RAS_spaces_occ) - dimension/number of orbitals in each of the occupied RAS space (eg. dimension_RAS_space_occ(1)=number of orbitals in the first RAS space
+  !dimension_active_space - number of orbitals in the active space 
+  
+  !check if number of all the orbitals is equal to norb
+
+  orb=0
+  do i=1, n_RAS_spaces_occ
+     orb=orb+RAS_space_occ(i)
+  end do
+
+  orb=orb+active_space
+  
+  do i=1, n_RAS_spaces_virt
+     orb=orb+RAS_space_virt(i)
+  end do
+
+  if (abs(n_RAS_spaces_occ-n_RAS_spaces_virt)>0) then
+     write(*,*)"NUMBER of RAS SPACE OCC MUST BE EQUAL to the NUMBER of RAS SPACE VIRT",n_RAS_spaces_occ,n_RAS_spaces_virt
+     call flush(6)
+     STOP
+  end if
+
+  if (orb .ne. norb) then
+     write(*,*)"SOMETHING WENT WRONG,number of orbitals declared in RAS spaces or active space is wrong (number of orbitals, number or orbitals in RAS+ACTIVE)", norb, orb
+     call flush(6)
+     do i=1, n_RAS_spaces_occ
+        write(*,*)"RAS SPACE OCC",i,"number of orbitals", RAS_space_occ(i)
+        call flush(6)
+     end do
+
+     write(*,*)"Active_space",active_space
+     call flush(6)
+     
+     
+
+     do i=1, n_RAS_spaces_virt
+        write(*,*)"RAS SPACE VIRT",i,"number of orbitals", RAS_space_virt(i)
+        call flush(6)
+     end do
+
+     write(*,*)"QUITING THE PROGRAM"
+     call flush(6)
+     STOP
+     
+  end if
+
+  !check if number of all the n_alpha is smaller or equal to orb
+  
+  if (orb<n_alpha) then
+     write(*,*)"SOMETHING WENT WRONG,number of declared alpha electrons", n_alpha, "is greater than total number of orbitals", orb
+     call flush(6)
+     write(*,*)"QUITING THE PROGRAM"
+     call flush(6)
+     STOP
+  end if
+
+
+  !check if number of all the n_beta is smaller or equal to orb
+  
+    if (orb<n_beta) then
+     write(*,*)"SOMETHING WENT WRONG,number of declared alpha electrons", n_beta, "is greater than total number of orbitals", orb
+     call flush(6)
+     write(*,*)"QUITING THE PROGRAM"
+     call flush(6)
+     STOP
+  end if
+
+end subroutine check_orbital_space_declarations
+
+
+
+
+subroutine fill_RAS_electron_array(relativistic,norb,n_RAS_spaces_occ,RAS_space_occ,n_RAS_spaces_virt,RAS_space_virt,active_space,excit_array,n_alpha,n_beta,NRD_spin_tmp,NRD_spin_alpha,NRD_spin_beta,RAS_el_array_alpha,RAS_el_array_beta)
+  
+  implicit none
+  logical, intent(in) :: relativistic
+  integer, intent(in) :: n_alpha, n_beta
+  integer, intent(in) :: norb,n_RAS_spaces_occ,n_RAS_spaces_virt,active_space
+  integer, intent(in) :: RAS_space_occ(n_RAS_spaces_occ),RAS_space_virt(n_RAS_spaces_virt)
+  integer, intent(in) :: excit_array(n_RAS_spaces_occ+n_RAS_spaces_virt)
+  integer, intent(inout):: NRD_spin_alpha,NRD_spin_beta
+  integer, intent(in) :: NRD_spin_tmp
+  integer, intent(inout):: RAS_el_array_alpha(NRD_spin_tmp,n_RAS_spaces_occ+n_RAS_spaces_virt+1),RAS_el_array_beta(NRD_spin_tmp,n_RAS_spaces_occ+n_RAS_spaces_virt+1)
+  integer:: orb,n_spaces
+
+
+  integer:: i,j,k,l
+  
+  !excit_array(n_RAS_spaces_occ+n_RAS_spaces_virt) - an array describing the excitation level possible in each of the RAS spaces
+  k=1
+  do i=1,n_RAS_spaces_occ
+     if (excit_array(i)>RAS_space_occ(i)) then
+        write(*,*)"Excitation level of RAS_space_occ",i,"greater than the size of the space", excit_array(i),RAS_space_occ(i)
+        call flush(6)
+        STOP
+     end if
+     k=k+1
+  end do
+  
+  do i=1, n_RAS_spaces_virt
+     if (excit_array(k)>RAS_space_virt(i)) then
+        write(*,*)"Excitation level of RAS_space_virt",i,"greater than the size of the space", excit_array(k),RAS_space_virt(i)
+        call flush(6)
+        STOP
+     end if
+     k=k+1
+  end do
+
+
+
+  
+  ! for alpha electrons
+  call fill_RAS_spaces_per_spin(n_alpha,NRD_spin_tmp,NRD_spin_alpha,n_RAS_spaces_occ,RAS_space_occ,n_RAS_spaces_virt,RAS_space_virt,active_space,excit_array,RAS_el_array_alpha)
+
+  
+  ! for beta electrons
+!  call fill_RAS_spaces_per_spin(n_beta,NRD_spin_tmp,NRD_spin_beta,n_RAS_spaces_occ,RAS_space_occ,n_RAS_spaces_virt,RAS_space_virt,active_space,excit_array,RAS_el_array_beta)
+
+  if (relativistic == .true.) then
+     
+  ! for alpha+1 electrons
+     call fill_RAS_spaces_per_spin(n_alpha+1,NRD_spin_tmp,NRD_spin_alpha,n_RAS_spaces_occ,RAS_space_occ,n_RAS_spaces_virt,RAS_space_virt,active_space,excit_array,RAS_el_array_alpha)
+    ! for alpha-1 electrons   
+       call fill_RAS_spaces_per_spin(n_alpha-1,NRD_spin_tmp,NRD_spin_alpha,n_RAS_spaces_occ,RAS_space_occ,n_RAS_spaces_virt,RAS_space_virt,active_space,excit_array,RAS_el_array_alpha)
+
+  
+  ! for beta+1 electrons
+!  call fill_RAS_spaces_per_spin(n_beta+1,NRD_spin_tmp,NRD_spin_beta,n_RAS_spaces_occ,RAS_space_occ,n_RAS_spaces_virt,RAS_space_virt,active_space,excit_array,RAS_el_array_beta)
+
+
+    ! for beta-1 electrons
+!  call fill_RAS_spaces_per_spin(n_beta-1,NRD_spin_tmp,NRD_spin_beta,n_RAS_spaces_occ,RAS_space_occ,n_RAS_spaces_virt,RAS_space_virt,active_space,excit_array,RAS_el_array_beta)
+
+end if
+
+
+end subroutine fill_RAS_electron_array
+  
+  
+subroutine fill_RAS_spaces_per_spin(n_spin,NRD_spin_tmp,NRD_spin,n_RAS_spaces_occ,RAS_space_occ,n_RAS_spaces_virt,RAS_space_virt,active_space,excit_array,RAS_el_array_spin)
+  implicit none
+  integer, intent(in) :: n_spin
+  integer, intent(in) :: n_RAS_spaces_occ,n_RAS_spaces_virt,active_space
+  integer, intent(in) :: RAS_space_occ(n_RAS_spaces_occ),RAS_space_virt(n_RAS_spaces_virt),excit_array(n_RAS_spaces_occ+n_RAS_spaces_virt)
+  integer, intent(in) :: NRD_spin_tmp
+  integer, intent(inout):: NRD_spin
+  integer, intent(inout):: RAS_el_array_spin(NRD_spin_tmp,n_RAS_spaces_occ+n_RAS_spaces_virt+1)
+
+  integer, dimension(:,:), allocatable::occ_RAS_el,virt_RAS_el
+  integer, dimension(:), allocatable::counter
+  integer:: n_el_temp
+  integer:: i,j,k,l
+  logical:: count_possibilities,fill
+  
+  !NRD_spin - number of possible electron distributions for a given alpha or beta spin
+  
+  
+  
+  do i=1,n_RAS_spaces_occ   
+     do j=excit_array(i),0,-1
+        do k=1,n_RAS_spaces_virt
+           do l=0,excit_array(n_RAS_spaces_occ+k),+1
+              n_el_temp=RAS_space_occ(i)-j+l
+              if (n_el_temp<=n_spin) then
+                 NRD_spin=NRD_spin+1
+!                 if (i==1)then
+
+!                 end if
+                 RAS_el_array_spin(NRD_spin,i)=RAS_space_occ(i)-j
+                 RAS_el_array_spin(NRD_spin,n_RAS_spaces_occ+1)=n_spin-n_el_temp
+                 RAS_el_array_spin(NRD_spin,n_RAS_spaces_occ+k+1)=l
+!                 write(*,*)"***",NRD_spin,i,RAS_space_occ(i)-j,n_spin-n_el_temp,k,l
+              end if
+           end do
+        end do
+     end do
+  end do
+
+  allocate(counter(n_RAS_spaces_occ+n_RAS_spaces_virt))
+  count_possibilities=.true.
+  call  count_el_nums_in_ras_spaces(count_possibilities,fill,counter,occ_RAS_el,virt_RAS_el,n_RAS_spaces_occ,n_RAS_spaces_virt,RAS_space_occ,excit_array)
+  
+
+!  allocate(occ_RAS_el(n_RAS_spaces_occ,MAXVAL(counter)))
+!  allocate(virt_RAS_el(n_RAS_spaces_virt,MAXVAL(counter)))
+
+!  fill=.true.
+!  call  count_el_nums_in_ras_spaces(count_possibilities,fill,counter,occ_RAS_el,virt_RAS_el,n_RAS_spaces_occ,n_RAS_spaces_virt,RAS_space_occ,excit_array)
+
+  
+end subroutine fill_RAS_spaces_per_spin
+
+subroutine count_el_nums_in_ras_spaces(count_possibilities,fill,counter,occ_RAS_el,virt_RAS_el,n_RAS_spaces_occ,n_RAS_spaces_virt,RAS_space_occ,excit_array)
+  implicit none
+  logical, intent(in)::count_possibilities,fill
+  integer, intent(in) :: n_RAS_spaces_occ,n_RAS_spaces_virt
+  integer, intent(in) :: RAS_space_occ(n_RAS_spaces_occ),excit_array(n_RAS_spaces_occ+n_RAS_spaces_virt)
+  integer, intent(inout) :: counter(n_RAS_spaces_occ+n_RAS_spaces_virt)
+  integer, intent(inout)::occ_RAS_el(:,:),virt_RAS_el(:,:)
+  integer:: i,j,k
+
+  
+ 
+  do i=1,n_RAS_spaces_occ   
+     k=0
+     do j=excit_array(i),0,-1
+        if (fill ==.true.) then
+           occ_RAS_el(i,k+1)=RAS_space_occ(i)-j
+           write(*,*)"OCC",i,RAS_space_occ(i)-j
+        end if
+        k=k+1
+     end do
+     if (count_possibilities == .true.) then
+        counter(i)=k
+        write(*,*)"OCCcounter",i,k
+     end if
+  end do
+
+
+  do i=1,n_RAS_spaces_virt
+     k=0
+     do j=0,excit_array(n_RAS_spaces_occ+i),+1
+        if (fill ==.true.) then 
+           virt_RAS_el(i,k)=j
+           write(*,*)"VIRT",n_RAS_spaces_occ+i,j
+        end if
+        k=k+1
+     end do
+     if (count_possibilities == .true.) then
+        counter(n_RAS_spaces_occ+i)=k
+        write(*,*)"VIRTcounter",n_RAS_spaces_occ+i,k
+     end if
+  end do
+  
+end subroutine count_el_nums_in_ras_spaces
+
+
+
+subroutine unique_elements(size,arr,uniq,count)
+    implicit none
+    integer, intent(in):: size
+    integer, intent(out)::count
+    integer, intent(in):: arr(size)
+    integer, intent(out)::uniq(size)
+    integer :: i, j, n
+    logical :: is_new
+
+    ! Example input array
+
+    count = 0
+
+    
+    do i = 1, size
+       write(*,*)"ARR",i,arr(i)
+       is_new = .true.
+        do j = 1, count
+            if (abs(arr(i)-arr(j))==0) then
+                is_new = .false.
+                exit
+            end if
+        end do
+
+        if (is_new) then
+            count = count + 1
+            uniq(count) = arr(i)
+        end if
+    end do
+
+    ! Print results
+    print *, 'Original array: ', arr
+    print *, 'Unique elements:', uniq(1:count)
+  end subroutine unique_elements
---
+++ main.f90
@@ -0,0 +1,101 @@
+program relatvistic_ed
+  implicit none
+  integer:: n_alpha,n_beta,norb
+  integer:: n_RAS_spaces_occ,n_RAS_spaces_virt,active_space
+  integer, dimension(:), allocatable:: RAS_space_occ,RAS_space_virt,excit_array
+  integer:: NRD_spin_alpha,NRD_spin_beta,NRD_spin_tmp
+  integer, dimension(:,:), allocatable:: RAS_el_array_alpha,RAS_el_array_beta
+  integer:: orb,n_spaces
+  logical:: relativistic
+  double precision, dimension(:), allocatable:: scratch
+  integer:: max_scratch
+  integer, dimension(:), allocatable:: scratch_int
+  integer:: max_scratch_int
+  
+  integer:: i,j,k,l
+  integer:: count_unique
+
+
+  !norb - total number of orbitals
+  ! the principles for the occupied and virtual RAS spaces are the same
+  !n_RAS_spaces_occ - the number of RAS spaces which are present for occupied orbitals (eg. 3 spaces with core, singly, and doubly occupied orbitals out of total number of occupied orbitals)
+  !RAS_space_occ(number_RAS_spaces_occ) - dimension/number of orbitals in each of the occupied RAS space (eg. dimension_RAS_space_occ(1)=number of orbitals in the first RAS space
+  !active_space - number of orbitals in the active space 
+  !excit_array(n_RAS_spaces_occ+n_RAS_spaces_virt) - an array describing the excitation level possible in each of the RAS spaces
+  !relativistic - logical that is switching between relativistic and nonrelativistic calculations (relativistic = .true. is a relativistic calculation)
+
+  max_scratch=10000000
+  allocate(scratch(max_scratch))
+  max_scratch_int=1000000
+  allocate(scratch_int(max_scratch_int))
+  !in a real program we will get these from the input
+  
+  relativistic=.true.
+  norb=14
+
+  n_alpha=6
+  n_beta=6
+  
+  n_RAS_spaces_occ=2
+  n_RAS_spaces_virt=2
+
+
+
+  allocate(RAS_space_occ(n_RAS_spaces_occ),RAS_space_virt(n_RAS_spaces_virt))
+
+
+  !in a real program we will get these from the input
+  
+  RAS_space_occ(1)=3
+  RAS_space_occ(2)=2
+  active_space=4
+  RAS_space_virt(1)=3
+  RAS_space_virt(2)=2
+
+  allocate(excit_array(n_RAS_spaces_occ+n_RAS_spaces_virt))
+  excit_array(:)=2
+  
+  
+  call check_orbital_space_declarations(norb,n_RAS_spaces_occ,RAS_space_occ,n_RAS_spaces_virt,RAS_space_virt,active_space,n_alpha,n_beta)
+
+  
+  !estimate maximal theoretical limit = product of el in occupied ras spaces*number of el in active_space*product of el in virtutal ras spaces
+
+  NRD_spin_tmp=1
+  do i=1,n_RAS_spaces_occ 
+     NRD_spin_tmp=NRD_spin_tmp*RAS_space_occ(i)
+  end do
+
+  NRD_spin_tmp=NRD_spin_tmp*active_space
+  do i=1, n_RAS_spaces_virt
+     NRD_spin_tmp=NRD_spin_tmp*RAS_space_virt(i)
+  end do
+
+  n_spaces=n_RAS_spaces_occ+1+n_RAS_spaces_virt !total number of RAS spaces+active space
+  allocate(RAS_el_array_alpha(NRD_spin_tmp,n_spaces))
+  allocate(RAS_el_array_beta(NRD_spin_tmp,n_spaces))
+
+  NRD_spin_alpha=0
+  NRD_spin_beta=0
+  
+  call fill_RAS_electron_array(relativistic,norb,n_RAS_spaces_occ,RAS_space_occ,n_RAS_spaces_virt,RAS_space_virt,active_space,excit_array,n_alpha,n_beta,NRD_spin_tmp,NRD_spin_alpha,NRD_spin_beta,RAS_el_array_alpha,RAS_el_array_beta)
+
+  k=1
+  do i=1,NRD_spin_alpha
+     do j=1,1 !n_RAS_spaces_virt+n_RAS_spaces_virt+1
+        write(*,*)"RAS_el_array_alpha",i,j,RAS_el_array_alpha(i,j)
+        scratch_int(k)=RAS_el_array_alpha(i,j)
+!        write(*,*)"scratch(k)",k,scratch(k)
+        k=k+1
+     end do
+  end do
+  STOP
+  !now try to find unique number of electrons in each of the spaces
+
+  call unique_elements(k-1,scratch_int(1:k-1),scratch_int(k),count_unique)
+  do i=1,count_unique
+     write(*,*)"unique el num",i,scratch_int(k+i)
+  end do
+  
+
+end program relatvistic_ed
