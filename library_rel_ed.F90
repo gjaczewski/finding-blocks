233c63238cac1609ca0d4bcca90202dd4e3c036d
gjaczewski <101567580+gjaczewski@users.noreply.github.com>
2026-07-13 17:01:08 +0200

    Add files via upload

--- library_rel_ed.F90
+++ library_rel_ed.F90
@@ -1589,9 +1589,9 @@ end do
 end if
 end subroutine create_hole
 
-subroutine diff_spin_product(ground_state,dane,new_state_alpha,new_dane_alpha,new_state_beta,new_dane_beta,scalar_product)
+subroutine diff_spin_product(new_state_alpha,new_dane_alpha,new_state_beta,new_dane_beta,scalar_product)
 type(t_params), intent(in) :: dane, new_dane_alpha, new_dane_beta
-complex(8), intent(in) :: ground_state(dane%size_tot(1,2)+dane%size_tot(2,2)+dane%size_tot(3,2)),new_state_alpha(new_dane_alpha%size_tot(1,2)+new_dane_alpha%size_tot(2,2)+new_dane_alpha%size_tot(3,2)),new_state_beta(new_dane_beta%size_tot(1,2)+new_dane_beta%size_tot(2,2)+new_dane_beta%size_tot(3,2))
+complex(8), intent(in) :: new_state_alpha(new_dane_alpha%size_tot(1,2)+new_dane_alpha%size_tot(2,2)+new_dane_alpha%size_tot(3,2)),new_state_beta(new_dane_beta%size_tot(1,2)+new_dane_beta%size_tot(2,2)+new_dane_beta%size_tot(3,2))
 complex(8), intent(out) :: scalar_product
 integer :: i
 scalar_product = 0
@@ -1620,14 +1620,103 @@ complex(8), intent(in) :: z
 integer, intent(in) :: orbital, krylov_size
 integer, intent(in) :: spin, e_or_h
 complex(8), intent(out) :: fraction
-complex(8) :: new_state(new_dane%size_tot(1,2)+new_dane%size_tot(2,2)+new_dane%size_tot(3,2)), a(krylov_size), b(krylov_size)
+complex(8) :: new_state1(new_dane%size_tot(1,2)+new_dane%size_tot(2,2)+new_dane%size_tot(3,2)),new_state2(new_dane%size_tot(1,2)+new_dane%size_tot(2,2)+new_dane%size_tot(3,2)),new_state3(new_dane%size_tot(1,2)+new_dane%size_tot(2,2)+new_dane%size_tot(3,2)),temp_state(new_dane%size_tot(1,2)+new_dane%size_tot(2,2)+new_dane%size_tot(3,2)) ,a(krylov_size), b(krylov_size)
+integer :: i,j
+
 if (e_or_h .eq. 1) then
- call create_electron(orbital,ground_state,new_state,dane,new_dane,spin)
+ call create_electron(orbital,ground_state,new_state1,dane,new_dane,spin)
 else if (e_or_h .eq. -1) then
- call create_hole(orbital,ground_state,new_state,dane,new_dane,spin)
+ call create_hole(orbital,ground_state,new_state1,dane,new_dane,spin)
+else if (e_or_h .eq. 2137) then
+ new_state1 = ground_state
 else
 write(*,*) "ERROR: e_or_h must be equal 1 (creation) or -1 (annihilation)"
 stop
 end if
+
+b(1) = 0
+do i=1,new_dane%size_tot(1,2)+new_dane%size_tot(2,2)+new_dane%size_tot(3,2)
+   b(1) = b(1) + abs(new_state1(i))**2
+end do
+b(1) = sqrt(b(1))
+
+
+new_state1 = new_state1/b(1)
+
+
+call matrix_vector_product(new_dane, new_state1, temp_state)
+
+a(1) = 0
+do i=1,new_dane%size_tot(1,2)+new_dane%size_tot(2,2)+new_dane%size_tot(3,2)
+   a(1) = a(1) + conjg(new_state1(i))*temp_state(i)
+end do
+
+new_state2 = temp_state - a(1)*new_state1
+
+b(2) = 0
+do i=1,new_dane%size_tot(1,2)+new_dane%size_tot(2,2)+new_dane%size_tot(3,2)
+   b(2) = b(2) + abs(new_state2(i))**2
+end do
+b(2) = sqrt(b(2))
+
+new_state2 = new_state2/b(2)
+call matrix_vector_product(new_dane, new_state2, temp_state)
+do i=3,krylov_size
+   new_state3 = temp_state - a(i-1)*new_state2 - b(i-1)*new_state1
+   b(i) = 0
+   do j=1,new_dane%size_tot(1,2)+new_dane%size_tot(2,2)+new_dane%size_tot(3,2)
+      b(i) = b(i) + abs(new_state3(i))**2
+   end do
+   b(i) = sqrt(b(i))
+   new_state3 = new_state3/b(i)
+   call matrix_vector_product(new_dane, new_state3, temp_state)
+   a(i) = 0
+   do j=1,new_dane%size_tot(1,2)+new_dane%size_tot(2,2)+new_dane%size_tot(3,2)
+      a(i) = a(i) + conjg(new_state3(j))*temp_state(j)
+   end do
+   new_state1 = new_state2
+   new_state2 = new_state3
+end do
+
+fraction = 0
+do i=krylov_size,1,-1
+   fraction = b(i)**2/(z - a(i) - fraction)
+end do
+end subroutine calc_fraction_diag
+
+
+subroutine calc_gf_matrix_element(ground_state,dane,new_dane1,new_dane2,z,orbital1,orbital2,spin1,spin2,e_or_h,krylov_size,gf_matrix_element)
+type(t_params), intent(in) :: dane, new_dane1, new_dane2
+complex(8), intent(in) :: ground_state(dane%size_tot(1,2)+dane%size_tot(2,2)+dane%size_tot(3,2))
+complex(8), intent(in) :: z
+integer, intent(in) :: orbital1, orbital2, krylov_size
+integer, intent(in) :: spin1, spin2, e_or_h
+complex(8), intent(out) :: gf_matrix_element
+complex(8) :: fraction_plus, fraction1, fraction2
+complex(8) :: new_state1(new_dane1%size_tot(1,2)+new_dane1%size_tot(2,2)+new_dane1%size_tot(3,2)), new_state2(new_dane2%size_tot(1,2)+new_dane2%size_tot(2,2)+new_dane2%size_tot(3,2))
+complex(8) :: temp_state(new_dane1%size_tot(1,2)+new_dane1%size_tot(2,2)+new_dane1%size_tot(3,2))
+if (e_or_h .eq. 1) then
+ call create_electron(orbital1,ground_state,new_state1,dane,new_dane1,spin1)
+ call create_electron(orbital2,ground_state,new_state2,dane,new_dane2,spin2)
+else if (e_or_h .eq. -1) then
+ call create_hole(orbital1,ground_state,new_state1,dane,new_dane1,spin1)
+ call create_hole(orbital2,ground_state,new_state2,dane,new_dane2,spin2)
+else
+write(*,*) "ERROR: e_or_h must be equal 1 (creation) or -1 (annihilation)"
+stop
+end if
+
+if (spin1 .eq. spin2) then
+   temp_state = new_state1 + new_state2
+   call calc_fraction_diag(temp_state,new_dane1,new_dane1,z,orbital1,spin1,2137,krylov_size,fraction_plus)
+else
+   !!!!!!!!!!!!!
+end if
+
+call calc_fraction_diag(ground_state,dane,new_dane1,z,orbital1,spin1,e_or_h,krylov_size,fraction1) 
+call calc_fraction_diag(ground_state,dane,new_dane2,z,orbital2,spin2,e_or_h,krylov_size,fraction2)
+
+gf_matrix_element = 0.5*(fraction_plus - fraction1 - fraction2)
+end subroutine calc_gf
 end module library_rel_ed
 
