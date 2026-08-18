01c04fb1220b54821a495cbf825dfca229c2e4cc
gjaczewski <101567580+gjaczewski@users.noreply.github.com>
2026-08-18 16:50:30 +0200

    Add files via upload

--- main.F90
+++ main.F90
@@ -13,7 +13,7 @@ logical:: relativistic
 complex(8), allocatable :: hopping_alpha(:,:),hopping_beta(:,:),interaction_alpha(:,:,:,:),interaction_beta(:,:,:,:),interaction_mix(:,:,:,:), hso_ab(:,:), hso_ba(:,:)
 real(8), allocatable :: orbital_energies(:)
 integer :: krylov_size
-integer :: n_pairs = 5
+integer :: n_pairs = 6
 
 real(8) :: nuclear_energy
 !*********HERE ARE OTHER VARIABLES*********
@@ -37,7 +37,7 @@ call cpu_time(start_time)
 
 !*******HERE USER DEFINES VARIABLES********
 
-relativistic=.false.
+relativistic=.true.
 n_orb = 2
 n_alpha = 1
 n_beta = 1
@@ -106,18 +106,24 @@ do i=1,n_orb
         p = p + 1
     end do
 end do
-!interaction_alpha(:,:,:,:) = 0
+interaction_alpha(:,:,:,:) = 0
+interaction_alpha(1,1,1,1) = 4
+interaction_alpha(2,2,2,2) = 4
 interaction_beta = interaction_alpha
 interaction_mix = interaction_alpha
 !interaction_mix(1,1,1,1) = 4
 !interaction_mix(2,2,2,2) = 4
 nuclear_energy = 0 
-!hopping_alpha(1,1)=-5
-!hopping_alpha(2,2)=-8
-!hopping_alpha(1,2)=0
-!hopping_alpha(2,1)=0
-!hopping_beta = hopping_alpha
-
+hopping_alpha(1,1)=5
+hopping_alpha(2,2)=5
+hopping_alpha(1,2)=-1
+hopping_alpha(2,1)=-1
+hopping_beta = hopping_alpha
+hso_ab(1,1) = 0
+hso_ab(2,2) = 0
+hso_ab(1,2) = (0.0,0.3)
+hso_ab(2,1) = (0.0,-0.3)
+hso_ba = hso_ab
 call generate_params(gs_params,relativistic,n_orb,n_alpha,n_beta,n_RAS_spaces_occ,n_RAS_spaces_virt,RAS_space_occ,RAS_space_virt,active_space,excit_array,orbital_energies,hopping_alpha,hopping_beta,interaction_alpha,interaction_beta, interaction_mix, hso_ab, hso_ba)
 gs_params%nuclear_energy = nuclear_energy
 N = gs_params%size_tot(1,2) + gs_params%size_tot(2,2) + gs_params%size_tot(3,2)
@@ -135,12 +141,13 @@ deallocate(eigenstates)
 
 
 
-call generate_params(params2,relativistic,n_orb,n_alpha+1,n_beta,n_RAS_spaces_occ,n_RAS_spaces_virt,RAS_space_occ,RAS_space_virt,active_space,excit_array,orbital_energies,hopping_alpha,hopping_beta,interaction_alpha,interaction_beta, interaction_mix, hso_ab, hso_ba)
+call generate_params(params2,relativistic,n_orb,n_alpha,n_beta-1,n_RAS_spaces_occ,n_RAS_spaces_virt,RAS_space_occ,RAS_space_virt,active_space,excit_array,orbital_energies,hopping_alpha,hopping_beta,interaction_alpha,interaction_beta, interaction_mix, hso_ab, hso_ba)
 params2%nuclear_energy = nuclear_energy
 N = params2%size_tot(1,2) + params2%size_tot(2,2) + params2%size_tot(3,2)
-allocate(eigenenergies(2))
-allocate(eigenstates(2,N))
-call eigensystem(params2,2,eigenenergies,eigenstates,ierr)
+allocate(eigenenergies(4))
+allocate(eigenstates(4,N))
+call eigensystem(params2,4,eigenenergies,eigenstates,ierr)
+
 allocate(gf1(n_orb,n_orb))
 allocate(gf2(n_orb,n_orb))
 !HERE WE START GENERATING ELECTRONIC GREEN FUNCTION
@@ -149,9 +156,9 @@ krylov_size = 1000
 !call generate_params(gf_params,relativistic,n_orb,n_alpha+1,n_beta,n_RAS_spaces_occ,n_RAS_spaces_virt,RAS_space_occ,RAS_space_virt,active_space,excit_array,orbital_energies,hopping_alpha,hopping_beta,interaction_alpha,interaction_beta,interaction_mix, hso_ab, hso_ba)
 gf1(:,:) = 0
 gf2(:,:) = 0
-allocate(new_states(2,2))
+allocate(new_states(4,4))
 
-omega = (1.0d0,1.0d0)
+omega = (1.0d0,0.05d0)
 !call create_electron(1,[(0.0d0,0.0d0),(0.0d0,0.0d0),(0.0d0,0.0d0),(1.0d0,0.0d0)],new_state1,gs_params,params2,1)
 do i=1,n_orb
     call create_electron(i,ground_state,new_states(i,:),gs_params,params2,1)
